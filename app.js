7f89492f2dd5f5a9c7b6771e46ddcfe1944ed70f
Chitsanupong <first30952@gmail.com>
2021-10-17 11:59:10 +0700

    feat: add send emal via nodemailer

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const { ESRCH } = require('constants');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
+const path = require('path');
+require('dotenv').config({ path: './.env' });
 
 const app = express();
 
@@ -8,12 +11,44 @@ const app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+// static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 // body parser middleware (body parser is deprecated for express v 4.16 +)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.render('contact', { layout: false });
+});
+
+app.post('/send-email', async (req, res) => {
+  try {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: 'hotmail',
+      auth: {
+        user: process.env.EMAIL_USERNAME, // generated ethereal user
+        pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: `Chitsanupong <${process.env.EMAIL_USERNAME}>`, // sender address
+      to: 'c.tangvasinkul@gmail.com', // list of receivers
+      subject: 'Sending Email with Node.js', // Subject line
+      // text: 'Hello world?', // plain text body
+      html: '<b>Hello Chitsanupong!</b>', // html body
+    });
+
+    console.log('Sent: ' + info.response);
+
+    console.log('Message sent: %s', info.messageId);
+    res.send({ message: 'OK' }).status(200);
+  } catch (error) {
+    res.send({ error: error }).status(500);
+  }
 });
 
 app.listen(4000, () => console.log('Listening on Port 4000'));
